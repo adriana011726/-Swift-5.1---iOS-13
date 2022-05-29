ec08493e754d0f82a8d4ebd32d40cf1517f69615
Adriana Oliveira <adrianadesouzacorreia@gmail.com>
2022-05-29 19:23:09 -0300

    Add files via upload

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by ADRIANA DE SOUZA CORREIA DE OLIVEIRA on 27/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
